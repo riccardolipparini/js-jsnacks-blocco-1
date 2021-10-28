4458e51d570add55bdc2b15f19865c0e01f9d61d
Riccardo <riccardolipparini0@gmail.com>
2021-10-28 15:57:24 +0200

    secondo esercizio finito

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,18 +1,25 @@
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
-// collego il div ad una costante
+// prendere in imput le due parole
+// stabilire quale delle parole è piu lunga
+// utilizzo lenght sulle parole per confrontarle
+
+
+
+
+
+
+
 const contain = document.getElementById("container");
 
 // creo due costanti prompt
 const parolaUno = prompt("inserischi parola");
 const parolaDue = prompt("inserisci parola");
 
-// stabilisco l'if
-if(parolaUno > parolaDue){
-    contain.innerHTML += `${parolaDue}`
-    contain.innerHTML += `${parolaUno}`
-} else if(parolaUno < parolaDue){
-    contain.innerHTML += `${parolaUno}`
-    contain.innerHTML += `${parolaDue}`
-} 
+if(parolaUno.length > parolaDue.length) {
+    contain.innerHTML = `${parolaDue} ${parolaUno}`
+}else if(parolaUno.length <= parolaDue.length) {
+    contain.innerHTML = `${parolaUno} ${parolaDue}`
+}
+
